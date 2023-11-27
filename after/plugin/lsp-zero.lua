a1021c557a0d6bf3971d950ec0a87065fe2a87d5
Fabio Capocasale <rionhacker@gmail.com>
2023-11-27 18:52:16 +0100

    added pyrigh as lsp for python

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -20,3 +20,4 @@ require('mason-lspconfig').setup({
 require('lspconfig').lua_ls.setup({})
 require('lspconfig').rust_analyzer.setup({})
 require('lspconfig').jdtls.setup({})
+require('lspconfig').pyright.setup({})
