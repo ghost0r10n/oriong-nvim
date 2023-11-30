f8edeb771b82287c797ffaa6fa4d81c6a58b6338
Fabio Capocasale <rionhacker@gmail.com>
2023-11-30 13:23:50 +0100

    shell support plus python support

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,27 +1,24 @@
-
 require('lsp-zero.cmp-mapping')
 
 
 
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
-  -- The command that starts the language server
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-  cmd = {
+	-- The command that starts the language server
+	-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+	cmd = {
 
-    'java',     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-configuration', '/home/ghost0r10n/.local/share/nvim/mason/bin/jdtls',
-   '-javaagent', '~/configuration_folder/jv/lombok.jar',
-  },
+		'java', '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+		'-Dosgi.bundles.defaultStartLevel=4',
+		'-Declipse.product=org.eclipse.jdt.ls.core.product',
+		'-Dlog.protocol=true',
+		'-Dlog.level=ALL',
+		'-Xmx1g',
+		'--add-modules=ALL-SYSTEM',
+		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+		'-configuration', '/home/ghost0r10n/.local/share/nvim/mason/bin/jdtls',
+		'-javaagent', '~/configuration_folder/jv/lombok.jar',
+	},
 
 }
-
-
